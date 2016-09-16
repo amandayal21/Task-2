c9de2085383630eb8637512cbc09522da9962281
Aman Dayal <12amandayal12@gmail.com>
2016-09-16 18:48:57 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title> My First Task</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	</head>
+	<body style="background-color:#75C38A";>
+		<div class="container">
+			<div class="jumbotron">
+				<h2 style="font-family:monotype corosiva;"><i>Login Form</i>
+				</h2>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4">
+					<form>
+    					<div class="form-group">
+      						<label for="email"><b><i>Email:  </i></b></label>
+      						<input type="email" class="form-control" id="email" placeholder="Enter email">
+					    </div>
+					    <div class="form-group">
+					     
+					        <label for="pwd"><b><i>Password:  </i></label>
+					   
+					      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+					    </div>
+					    <div class="form-group">
+					    	<button type="submit" class="btn btn-primary form-group">Submit</button></div>
+					    <div class="form-group">
+					    	<label for="pwd">Don't have your Account?</label>
+					    	<a href="#" class="btn btn-danger btn-DEFAULT active" role="button">Sign Up</a>
+					    </div>
+					 </form>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+	</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<title> My First Task</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	</head>
+	<body style="background-color:orange";>
+		<div class="container">
+			<div class="jumbotron">
+				<h2 style="font-family:monotype corosiva;"><i>Login Form</i>
+				</h2>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4">
+					<form>
+    					<div class="form-group">
+      						<label for="USERNAME"><b><i>USERNAME:  </i></b></label>
+      						<input type="text" class="form-control" id="username" placeholder="username">
+					    </div>
+					    <div class="form-group">
+					     
+					        <label for="email"><b><i>EMAIL: </i></b></label>
+					   
+					      <input type="email" class="form-control" id="email" placeholder="Enter Email">
+					    </div>
+					    <div class="form-group">
+					    	<label for="password"><b><i>PASSWORD: </i></b></label>
+					    	<input type="password" class="form-control" id="pwd" placeholder="Enter Password"></label>
+
+					    	<button type="submit" class="btn btn-primary form-group">Submit</button></div>
+					    <div class="form-group">
+					    	<label for="pwd">Already having an Account?</label>
+					    	<a href="#" class="btn btn-danger btn-DEFAULT active" role="button">Sign In</a>
+					    </div>
+					 </form>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+	</body>
+
+</html>
